b987cba11e98661577e7ee73814736ae2a33676f
Ronney Arias <ronneyarias@gmail.com>
2023-10-24 13:46:15 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const farmAnimals = 'cow horse sheep pig chicken';
+const animalArray = farmAnimals.split(' ');
+const [moo, neigh, baa, oink, cluck] = animalArray;
+
+const traditionalNames = farmAnimals.split(' ');
+const [bessie, ,dolly, babe, little] = traditionalNames;
+
+const traditionalColors = farmAnimals.split(' ');
+const [blackAndWhite, ,black, pink, ,] = traditionalColors;
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r, o, y, g, b, i, v] = colors;
+const [, , , ,, indg,] = colors;
+
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,6 +21,8 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+
+const { muppetName, color, song, job, partner } = muppet;
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +38,8 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+
+const { album: { theMuppetMovie: { song2, song4 } }, nestedJob, nestedPartner } = nestedMuppet;
 
 // Strings
 
